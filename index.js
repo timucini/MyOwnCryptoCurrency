5ddeca810861eb210b6faa031d5727d85c9c985a
Burkholz <TBurkholz@gasag.de>
2019-10-13 16:03:07 +0200

    index hotfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PubSub = require('./app/redisPattern');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const app = express();
-const isDevelopment = process.env.ENV === 'development'
+const isDevelopment = process.env.ENV === 'development';
 const REDIS_URL = isDevelopment ? 'redis://127.0.0.1:6379' : 'redis://h:pa427241f95c8f6ac9722fc24f51b5ab59976a57e7ba20f4e8a0e835c6adce5f9@ec2-3-230-5-223.compute-1.amazonaws.com:6739'
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
